1709c3b0886f18c4816ccecdfe45d481dc354691
Vijay Vasanthapuram <vijay.vasanthapuram@arris.com>
2018-12-31 18:11:32 -0500

    Add Quiz component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component } from 'react';
+import './App.css';
+import './Quiz';
+import Quiz from "./Quiz";
 
 class App extends Component {
   render() {
     return (
-      <div />
+      <Quiz />
     )
   }
 }
 
-export default App
+export default App;
